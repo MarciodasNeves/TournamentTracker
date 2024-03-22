089dba54b65cf4676280e3b300e6475a7d1fb1f4
MarciodasNeves <rosamarciomajor@gmail.com>
2024-03-22 01:13:24 +0000

    creation fourth form (prize form)

---
+++ TournamentTracker/TrackerUI/CreatePrizeForm.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace TrackerUI
+{
+    partial class CreatePrizeForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            CreatePrizeLabel2 = new Label();
+            PlaceNumberValue = new TextBox();
+            PlaceNumberLabel = new Label();
+            PlaceNameValue = new TextBox();
+            PlaceNamelabel1 = new Label();
+            PriceAmountValue = new TextBox();
+            PriceAmountlabel1 = new Label();
+            PricePercentageValue = new TextBox();
+            PricePercentagelabel1 = new Label();
+            orlabel1 = new Label();
+            CreatePrizeButtom = new Button();
+            SuspendLayout();
+            // 
+            // CreatePrizeLabel2
+            // 
+            CreatePrizeLabel2.AutoSize = true;
+            CreatePrizeLabel2.Font = new Font("Segoe UI Light", 27.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            CreatePrizeLabel2.ForeColor = Color.FromArgb(51, 153, 255);
+            CreatePrizeLabel2.Location = new Point(12, 9);
+            CreatePrizeLabel2.Name = "CreatePrizeLabel2";
+            CreatePrizeLabel2.Size = new Size(209, 50);
+            CreatePrizeLabel2.TabIndex = 31;
+            CreatePrizeLabel2.Text = "Create Prize";
+            // 
+            // PlaceNumberValue
+            // 
+            PlaceNumberValue.Location = new Point(255, 98);
+            PlaceNumberValue.Name = "PlaceNumberValue";
+            PlaceNumberValue.Size = new Size(152, 35);
+            PlaceNumberValue.TabIndex = 33;
+            // 
+            // PlaceNumberLabel
+            // 
+            PlaceNumberLabel.AutoSize = true;
+            PlaceNumberLabel.Font = new Font("Segoe UI", 21.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            PlaceNumberLabel.ForeColor = Color.FromArgb(51, 153, 255);
+            PlaceNumberLabel.Location = new Point(15, 93);
+            PlaceNumberLabel.Name = "PlaceNumberLabel";
+            PlaceNumberLabel.Size = new Size(196, 40);
+            PlaceNumberLabel.TabIndex = 32;
+            PlaceNumberLabel.Text = "Place Number";
+            // 
+            // PlaceNameValue
+            // 
+            PlaceNameValue.Location = new Point(255, 156);
+            PlaceNameValue.Name = "PlaceNameValue";
+            PlaceNameValue.Size = new Size(152, 35);
+            PlaceNameValue.TabIndex = 35;
+            // 
+            // PlaceNamelabel1
+            // 
+            PlaceNamelabel1.AutoSize = true;
+            PlaceNamelabel1.Font = new Font("Segoe UI", 21.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            PlaceNamelabel1.ForeColor = Color.FromArgb(51, 153, 255);
+            PlaceNamelabel1.Location = new Point(15, 151);
+            PlaceNamelabel1.Name = "PlaceNamelabel1";
+            PlaceNamelabel1.Size = new Size(168, 40);
+            PlaceNamelabel1.TabIndex = 34;
+            PlaceNamelabel1.Text = "Place Name";
+            // 
+            // PriceAmountValue
+            // 
+            PriceAmountValue.Location = new Point(255, 214);
+            PriceAmountValue.Name = "PriceAmountValue";
+            PriceAmountValue.Size = new Size(152, 35);
+            PriceAmountValue.TabIndex = 37;
+            // 
+            // PriceAmountlabel1
+            // 
+            PriceAmountlabel1.AutoSize = true;
+            PriceAmountlabel1.Font = new Font("Segoe UI", 21.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            PriceAmountlabel1.ForeColor = Color.FromArgb(51, 153, 255);
+            PriceAmountlabel1.Location = new Point(15, 209);
+            PriceAmountlabel1.Name = "PriceAmountlabel1";
+            PriceAmountlabel1.Size = new Size(189, 40);
+            PriceAmountlabel1.TabIndex = 36;
+            PriceAmountlabel1.Text = "Price Amount";
+            // 
+            // PricePercentageValue
+            // 
+            PricePercentageValue.Location = new Point(255, 345);
+            PricePercentageValue.Name = "PricePercentageValue";
+            PricePercentageValue.Size = new Size(152, 35);
+            PricePercentageValue.TabIndex = 39;
+            // 
+            // PricePercentagelabel1
+            // 
+            PricePercentagelabel1.AutoSize = true;
+            PricePercentagelabel1.Font = new Font("Segoe UI", 21.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            PricePercentagelabel1.ForeColor = Color.FromArgb(51, 153, 255);
+            PricePercentagelabel1.Location = new Point(15, 340);
+            PricePercentagelabel1.Name = "PricePercentagelabel1";
+            PricePercentagelabel1.Size = new Size(227, 40);
+            PricePercentagelabel1.TabIndex = 38;
+            PricePercentagelabel1.Text = "Price Percentage";
+            // 
+            // orlabel1
+            // 
+            orlabel1.AutoSize = true;
+            orlabel1.Font = new Font("Segoe UI", 21.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            orlabel1.ForeColor = Color.FromArgb(51, 153, 255);
+            orlabel1.Location = new Point(162, 268);
+            orlabel1.Name = "orlabel1";
+            orlabel1.Size = new Size(80, 40);
+            orlabel1.TabIndex = 40;
+            orlabel1.Text = "-OR-";
+            // 
+            // CreatePrizeButtom
+            // 
+            CreatePrizeButtom.FlatStyle = FlatStyle.Flat;
+            CreatePrizeButtom.Font = new Font("Segoe UI Semibold", 15.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            CreatePrizeButtom.ForeColor = Color.FromArgb(51, 153, 255);
+            CreatePrizeButtom.Location = new Point(152, 482);
+            CreatePrizeButtom.Name = "CreatePrizeButtom";
+            CreatePrizeButtom.Size = new Size(255, 58);
+            CreatePrizeButtom.TabIndex = 45;
+            CreatePrizeButtom.Text = "Create Prize";
+            CreatePrizeButtom.UseVisualStyleBackColor = true;
+            // 
+            // CreatePrizeForm
+            // 
+            AutoScaleDimensions = new SizeF(12F, 30F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.White;
+            ClientSize = new Size(468, 698);
+            Controls.Add(CreatePrizeButtom);
+            Controls.Add(orlabel1);
+            Controls.Add(PricePercentageValue);
+            Controls.Add(PricePercentagelabel1);
+            Controls.Add(PriceAmountValue);
+            Controls.Add(PriceAmountlabel1);
+            Controls.Add(PlaceNameValue);
+            Controls.Add(PlaceNamelabel1);
+            Controls.Add(PlaceNumberValue);
+            Controls.Add(PlaceNumberLabel);
+            Controls.Add(CreatePrizeLabel2);
+            Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            Margin = new Padding(5, 6, 5, 6);
+            Name = "CreatePrizeForm";
+            Text = "Create Prize";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label CreatePrizeLabel2;
+        private TextBox PlaceNumberValue;
+        private Label PlaceNumberLabel;
+        private TextBox PlaceNameValue;
+        private Label PlaceNamelabel1;
+        private TextBox PriceAmountValue;
+        private Label PriceAmountlabel1;
+        private TextBox PricePercentageValue;
+        private Label PricePercentagelabel1;
+        private Label orlabel1;
+        private Button CreatePrizeButtom;
+    }
+}
